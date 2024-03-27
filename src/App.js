89636a112fbde9931be6eeb6406686ab899eb158
NANDA\nanda <nandaraditya80@gmail.com>
2024-03-27 10:32:48 +0700

    change path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   }, []);
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/portfolio-nanda" element={<Layout />}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
